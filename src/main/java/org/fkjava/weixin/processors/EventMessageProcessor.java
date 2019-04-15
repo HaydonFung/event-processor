20c7c5739f32f228303cad1e5434f2e3f6626064
Hasee <Hasee@NaNi>
2019-04-15 21:57:11 +0800

    新建

---
+++ src/main/java/org/fkjava/weixin/processors/EventMessageProcessor.java
@@ -0,0 +1,9 @@
+package org.fkjava.weixin.processors;
+
+import org.fkjava.weixin.domain.event.EventInMessage;
+
+public interface EventMessageProcessor {
+
+	void onMessage(EventInMessage event);
+
+}
---
+++ src/main/java/org/fkjava/weixin/processors/impl/SubscribeEventMessageProcessor.java
@@ -0,0 +1,16 @@
+package org.fkjava.weixin.processors.impl;
+
+import org.fkjava.weixin.domain.event.EventInMessage;
+import org.fkjava.weixin.processors.EventMessageProcessor;
+import org.springframework.stereotype.Service;
+
+// 把对象加入Spring容器，并且根据事件处理查找Bean的规则给一个名字
+@Service("subscribeMessageProcessor")
+public class SubscribeEventMessageProcessor implements EventMessageProcessor {
+
+	@Override
+	public void onMessage(EventInMessage event) {
+		System.out.println("处理关注消息： " + event);
+	}
+
+}
